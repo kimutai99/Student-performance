76edeb6b5e28ed26a81fa6d09bdc5c94b80c79c5
brian kimutai <korosbrian574@gmail.com>
2024-11-08 10:57:31 +0300

    data_ingstion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -12,7 +12,7 @@ class DataIngestionConfig:
     train_data_path:str =os.path.join('artifacts',"train.csv")
     test_data_path:str=os.path.join('artifacts',"test.csv")
     raw_data_path:str=os.path.join('artifacts',"data.csv")
-    
+
 class DataIngestion:
     def __init__ (self) :
         self.ingestion_config=DataIngestionConfig()
